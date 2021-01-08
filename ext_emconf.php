2cdefa96c63087580401fadd51ca59275d9ab459
David Steeb <david.steeb@b13.de>
2021-01-08 15:28:23 +0100

    [TASK] Add support for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.9.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
     ],
 ];
