45ae62d572b49fd177eab03a52a164447826d0f3
David Steeb <david.steeb@b13.com>
2022-11-08 09:48:27 +0100

    [BUGFIX] Use crop scaling for variants
    Resolves: #16

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -246,8 +246,8 @@ class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
                 $srcsetWidth = $variant;
                 $srcsetHeight = ($ratio ? $variant * (1 / $ratio) : null);
                 $srcsetProcessingInstructions = [
-                    'width' => $srcsetWidth,
-                    'height' => $srcsetHeight,
+                    'width' => $srcsetWidth . (strpos($this->arguments['width'], 'c') ? 'c' : ''),
+                    'height' => $srcsetHeight . (strpos($this->arguments['height'], 'c') ? 'c' : ''),
                     'minWidth' => null,
                     'minHeight' => null,
                     'maxWidth' => null,
