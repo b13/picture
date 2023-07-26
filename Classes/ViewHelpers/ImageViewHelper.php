0d5c8414c5ac847b2230fdbca3c1f4192b44e79a
David Steeb <david.steeb@b13.com>
2023-07-26 17:50:14 +0200

    [TASK] Update condition

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -150,7 +150,7 @@ class ImageViewHelper extends AbstractTagBasedViewHelper
                 $sourceOutputs = [];
 
                 // use src from sourceConfiguration, if set, otherwise use the main image
-                if ((string)$sourceConfiguration['src'] || (string)$sourceConfiguration['image']) {
+                if ((string)$sourceConfiguration['src'] || $sourceConfiguration['image'] ?? false) {
                     $imageSrc = $this->imageService->getImage(
                         (string)$sourceConfiguration['src'],
                         $sourceConfiguration['image'],
