5280ba58af98f77507861b93303babebe0b1e6b9
Georg Ringer <g.ringer@fonda.at>
2025-12-22 12:05:06 +0100

    [TASK] Allow TYPO3 14

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-13.99.99',
+            'typo3' => '11.5.0-14.99.99',
         ],
     ],
 ];
